d66645148712a75b4c7dd7cb3d32a47ff6dff720
shanmukh <shanmukh@outlook.com>
2023-09-17 08:44:56 -0400

    Initial Commit

---
+++ WritingPad/WritingPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  WritingPadApp.swift
+//  WritingPad
+//
+//  Created by Shanmukh Sista on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WritingPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WritingPadUITests/WritingPadUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WritingPadUITestsLaunchTests.swift
+//  WritingPadUITests
+//
+//  Created by Shanmukh Sista on 9/17/23.
+//
+
+import XCTest
+
+final class WritingPadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
